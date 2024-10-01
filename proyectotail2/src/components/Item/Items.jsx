914ddedc30ba895a7396208eddb677c76d136a08
Chaiguata 1 <stevenbenjamin.pavez@alumnos.ulagos.cl>
2024-10-01 10:10:10 -0300

    sec

---
+++ proyectotail2/src/components/Item/Items.jsx
@@ -0,0 +1,10 @@
+export default function Items(){
+    const imgProducto = "./"
+
+    return (
+        <div className="felx flex-col items-center">
+            <img src={imgProducto} alt="Imagen Producto"/>
+            <h
+        </div>
+    )
+}
